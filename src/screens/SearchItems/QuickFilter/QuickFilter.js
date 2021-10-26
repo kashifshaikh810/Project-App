e5b32bb4f932a663588227b77b75598d86e81560
Kashif Sheikh <kashifshaikh910ssg@gmail.com>
2021-10-26 22:32:34 +0500

    Implement filter screen UI partially

--- src/screens/SearchItems/QuickFilter/QuickFilter.js
+++ src/screens/SearchItems/QuickFilter/QuickFilter.js
@@ -1,13 +1,30 @@
 import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, TextInput, Keyboard} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  Keyboard,
+  ScrollView,
+} from 'react-native';
 import {Styles} from './Styles';
 import CloseIcon from 'react-native-vector-icons/AntDesign';
+import ClockIcon from 'react-native-vector-icons/AntDesign';
 import SearchIcon from 'react-native-vector-icons/EvilIcons';
 import {responsiveScreenWidth} from '../../../Components/Utility/ResponsiveDimensions/Responsive';
+import ArrowRightIcon from 'react-native-vector-icons/MaterialIcons';
 
 const QuickFilter = props => {
   const [onFocus, setOnFocus] = useState(false);
   const [search, setSearch] = useState('');
+  const [data, setData] = useState([]);
+
+  const categoriesData = [
+    {name: 'Mobiles'},
+    {name: 'Vehicles'},
+    {name: 'Property for Sale'},
+    {name: 'Propert for Rent'},
+  ];
 
   const fFocus = () => {
     setOnFocus(true);
@@ -18,8 +35,16 @@ const QuickFilter = props => {
     Keyboard.dismiss();
   };
 
+  const onSubmit = () => {
+    data.push(search);
+    setData(data);
+    setSearch('');
+    setOnFocus(false);
+    Keyboard.dismiss();
+  };
+
   return (
-    <View style={Styles.container}>
+    <ScrollView style={Styles.container}>
       <View style={Styles.headerContainer}>
         <TouchableOpacity onPress={() => props.navigation.goBack()}>
           <CloseIcon name="close" size={25} />
@@ -42,17 +67,18 @@ const QuickFilter = props => {
               Styles.headerContent,
               {
                 borderColor: onFocus ? '#74e3dc' : '#183338',
-                width: onFocus ? 250 : 320,
+                width: onFocus ? 250 : '97%',
               },
             ]}>
             <SearchIcon name="search" size={22} />
             <TextInput
               placeholder="Find Cars, Mobile Phones and more"
-              style={[Styles.textInput, {width: onFocus ? 210 : 340}]}
+              style={[Styles.textInput, {width: onFocus ? 210 : '100%'}]}
               onFocus={() => fFocus()}
               returnKeyType="search"
               value={search}
               onChangeText={text => setSearch(text)}
+              onSubmitEditing={() => onSubmit()}
             />
           </View>
         </View>
@@ -88,8 +114,72 @@ const QuickFilter = props => {
         </View>
       </View>
 
+      {data.map((arr, index) => {
+        return (
+          <View
+            key={index}
+            style={{
+              flexDirection: 'row',
+              height: 35,
+              marginVertical: 10,
+              alignItems: 'center',
+            }}>
+            <View style={{width: 65, alignItems: 'center'}}>
+              <ClockIcon name="clockcircleo" size={20} color="#052e31" />
+            </View>
+            <Text>{arr}</Text>
+          </View>
+        );
+      })}
 
-    </View>
+      {data.length === 0 && (
+        <View
+          style={{
+            flexDirection: 'row',
+            height: 35,
+            marginVertical: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text>No Recent Search</Text>
+        </View>
+      )}
+
+      <View
+        style={{
+          backgroundColor: '#eaeeef',
+          height: 50,
+          justifyContent: 'center',
+        }}>
+        <View style={{width: 190, alignItems: 'center'}}>
+          <Text style={{fontWeight: 'bold', color: '#0b3135', fontSize: 16}}>
+            POPULAR CATEGORIES
+          </Text>
+        </View>
+      </View>
+
+      {categoriesData.map((data, index) => {
+        return (
+          <View style={{alignItems: 'center'}} key={index}>
+            <TouchableOpacity
+              style={{width: '92%', height: 45, justifyContent: 'space-evenly'}}
+              onPress={() => console.log(data.name)}>
+              <View style={{flexDirection: 'row'}}>
+                <Text>{data.name}</Text>
+                <View
+                  style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'flex-end',
+                  }}>
+                  <ArrowRightIcon name="keyboard-arrow-right" size={22} />
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+        );
+      })}
+    </ScrollView>
   );
 };
 
